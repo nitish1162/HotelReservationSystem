72e9449c57f135caee170f8a0cb779d36f734d87
nitish1162 <nitishkc789@gmail.com>
2021-12-21 08:45:47 +0530

    Added welcome message

---
+++ src/main/java/com/hotelreservationsystem/hotelreservationsystem/App.java
@@ -0,0 +1,13 @@
+package com.hotelreservationsystem.hotelreservationsystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/welcomeMessage/WelcomeMessage.java
@@ -0,0 +1,8 @@
+package welcomeMessage;
+
+public class WelcomeMessage {
+	public static void main(String[] args) {
+		System.out.println("Hello customers! Welcome to the Hotel Reservation System.");
+	}
+
+}
